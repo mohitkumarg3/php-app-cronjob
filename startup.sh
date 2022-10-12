345626b8f8b2a116eaa57f766c6d5ca07ca5d183
Mohit Kumar <89012914+mohitkumarg3@users.noreply.github.com>
2022-10-12 16:51:44 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 apt-get update -y && apt-get install -y cron
-crontab -l ; echo '* * * * * /usr/local/bin/php -f ${HOME}/site/wwwroot/index.php' | crontab
+crontab -l ; echo '*/5 * * * * /usr/local/bin/php -f ${HOME}/site/wwwroot/index.php' | crontab
