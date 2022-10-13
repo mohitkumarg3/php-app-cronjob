caab29fb963809ed6564e696277b594e9899a4fe
Mohit Kumar <89012914+mohitkumarg3@users.noreply.github.com>
2022-10-13 16:40:15 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 apt update -y && apt install -y cron
-crontab -l ; echo "* * * * * `date` > /home/site/wwwroot/cron.log" | crontab
+crontab -l ; echo "* * * * * Hello World! > /home/site/wwwroot/cron.log" | crontab
