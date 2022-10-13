8291fb37416559c6035616ec27503af8394c4e0a
Mohit Kumar <89012914+mohitkumarg3@users.noreply.github.com>
2022-10-13 11:25:48 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 sudo apt-get update -y && apt-get install -y cron
-# crontab -l ; echo "2 * * * * echo "test $date" > home/site/wwwroot/cron.log" | crontab
-crontab -l;
-crontab echo "2 * * * * echo "test $date" > home/site/wwwroot/cron.log"
+crontab -l ; echo "2 * * * * echo "test $date" > home/site/wwwroot/cron.log" | crontab
+#crontab -l;
+#crontab echo "2 * * * * echo "test $date" > home/site/wwwroot/cron.log"
